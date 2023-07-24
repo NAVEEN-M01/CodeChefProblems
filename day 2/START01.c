00905d98ded8c9d5d6c47cdd66a77b035a24cab4
Naveen M <126571407+NAVEEN-M01@users.noreply.github.com>
2023-07-24 22:19:02 +0530

    Add files via upload

---
+++ day 2/START01.c
@@ -0,0 +1,15 @@
+/*              Number Mirror
+
+Write a program that takes a number N as the input, and prints it to the output.
+
+*/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int i;
+	scanf("%d",&i);
+	printf("%d",i);
+	return 0;
+}
+
