20a3d5eaf3786a10dd67e8710a343c26e6974eb1
Naveen M <126571407+NAVEEN-M01@users.noreply.github.com>
2023-07-26 23:55:40 +0530

    AGELIMIT.c

--- day 3/AGELIMIT.c
+++ day 3/AGELIMIT.c
@@ -7,10 +7,7 @@ int main(void) {
 	for(i=0;i<t;i++)
 	{
 	    scanf("%d%d%d",&x,&y,&a);
-	    if((x<=a)&&(y>a))
-	        printf("YES\n");
-	    else
-	         printf("NO\n");
+	    if((x<=a)&&(y>a))?printf("YES\n"):printf("NO\n");
 	}
 	return 0;
 }
